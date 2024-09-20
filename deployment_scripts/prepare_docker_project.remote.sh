2c737c54f03bee54bc4fbec024bc7bdd32e5c8e1
Jesse Abdul <jesse.abdul@noaa.gov>
2024-09-19 18:29:54 -1000

    updated the preparation scripts for each scenario to use the corresponding configuration values for the scenario and moved the common code into its own bash script

--- deployment_scripts/prepare_docker_project.remote.sh
+++ deployment_scripts/prepare_docker_project.remote.sh
@@ -1,87 +1,10 @@
 #! /bin/bash
 
-
-#deployment script for remote scenario
-echo "running remote scenario deployment script"
-
-# check if the base_docker_directory environment variable has been defined
-if [[ -z "${base_docker_directory}" ]]; then
-	# the base_docker_directory environment variable has not been defined
-
-	# prompt the user for the preparation folder base directory
-	echo "A \$base_docker_directory environment variable has not been defined."
-	echo "You must specify the base docker directory as an environment variable that will be used for the preparation folder (where the docker container will be built and executed from - e.g. /c/docker for Windows, /home/webd/docker for Linux)"
-
-	# define the base directory for the prepared working directory (the directory that will be used to build and execute the docker container)
-	read base_docker_directory
-
-fi 
-# the value of $base_docker_directory is defined, proceed with the preparation script
-
-echo "the value of \$base_docker_directory is defined, the docker container will be prepared for execution"
-
-
 # change directory to the folder this script is in to ensure the include .sh script reference is valid
 cd "$(dirname "$0")"
 
-# load the project configuration script to set the runtime variable values
-. ../docker/src/scripts/sh_script_config/project_deploy_config.sh
+# load the remote scenario configuration script to set the runtime variable values
+. ../docker/src/scripts/sh_script_config/project_scenario_config.remote.sh
 
-
-
-# construct the project folder name:
-project_folder_name=$project_path"-remote"
-
-# construct the full project path
-full_project_path=$base_docker_directory"/"$project_folder_name"/docker/src"
-
-# create the base directory
-mkdir -p $base_docker_directory
-
-#remove any files already in the $project_folder_name
-rm -rf $base_docker_directory/$project_folder_name
-
-# create the $project_folder_name
-mkdir $base_docker_directory/$project_folder_name
-
-echo "clone the project repository"
-
-#checkout the git projects into the same temporary docker directory
-git clone  $git_url $base_docker_directory/$project_folder_name
-
-echo "rename configuration files"
-
-#rename the query_metrics_calling_script.remote.sql to query_metrics_calling_script.sql so it can be used as the active configuration file
-mv $full_project_path/SQL/query_metrics_calling_script.remote.sql $full_project_path/SQL/query_metrics_calling_script.sql
-
-# remove the local and hybrid scripts
-rm $full_project_path/SQL/query_metrics_calling_script.local.sql
-
-rm $full_project_path/SQL/query_metrics_calling_script.hybrid.sql
-
-#rename the remote oracle configuration file to be the active configuration file
-mv $full_project_path/oracle_configuration/tnsnames.remote.ora $full_project_path/oracle_configuration/tnsnames.ora
-
-# remove the local and hybrid oracle configuration files
-rm $full_project_path/oracle_configuration/tnsnames.local.ora
-
-rm $full_project_path/oracle_configuration/tnsnames.hybrid.ora
-
-
-#rename the project_scenario_config.remote.sh to project_scenario_config.sh so it can be used as the active configuration file
-mv $full_project_path/scripts/sh_script_config/project_scenario_config.remote.sh $full_project_path/scripts/sh_script_config/project_scenario_config.sh
-
-# remove the remote and hybrid scripts
-rm $full_project_path/scripts/sh_script_config/project_scenario_config.local.sh
-
-rm $full_project_path/scripts/sh_script_config/project_scenario_config.hybrid.sh
-
-# remove the preparation scripts:
-rm $base_docker_directory"/"$project_folder_name"/deployment_scripts/prepare_docker_project.local.sh"
-rm $base_docker_directory"/"$project_folder_name"/deployment_scripts/prepare_docker_project.hybrid.sh"
-rm $base_docker_directory"/"$project_folder_name"/deployment_scripts/prepare_docker_project.remote.sh"
-
-echo ""
-echo "the remote docker project files are now ready for configuration and image building/deployment (press Enter key to continue)"
-
-read
+# execute the preparation script
+. ./prepare_docker_project.sh
---
+++ deployment_scripts/prepare_docker_project.sh
@@ -0,0 +1,99 @@
+#! /bin/bash
+
+# load the project configuration script to set the runtime variable values
+. ../docker/src/scripts/sh_script_config/project_deploy_config.sh
+
+# determine the scenario by using the values in the variables $database_location and $container_location
+if [[$database_location == "local"]] && [[$container_location == "local"]]; then
+	# this is a local database and container, this is a local scenario
+	testing_scenario="local"
+
+elif [[$database_location == "remote"]] && [[$container_location == "remote"]]; then
+	# this is a remote database and container, this is a remote scenario
+	testing_scenario="remote"
+
+else
+	# this is a remote database and local container, this is a hybrid scenario
+	testing_scenario="hybrid"
+fi
+
+
+#deployment script for local scenario
+echo "running $testing_scenario scenario deployment script"
+
+# check if the base_docker_directory environment variable has been defined
+if [[ -z "${base_docker_directory}" ]]; then
+	# the base_docker_directory environment variable has not been defined
+
+	# prompt the user for the preparation folder base directory
+	echo "A \$base_docker_directory environment variable has not been defined."
+	echo "You must specify the base docker directory that will be used as the directory that builds and executes the container (e.g. /c/docker for Windows, /home/webd/docker for Linux)"
+	echo 
+	echo 
+	echo "Specify the base docker directory: "
+
+	# define the base directory for the prepared working directory (the directory that will be used to build and execute the docker container)
+	read base_docker_directory
+
+fi 
+# the value of $base_docker_directory is defined, proceed with the preparation script
+
+echo "the value of \$base_docker_directory is \"$base_docker_directory\" that will be used to build and execute the docker container"
+
+
+# construct the project folder name based on the configuration variables:
+project_folder_name=$project_path"-"$testing_scenario
+
+# construct the full project path
+full_project_path=$base_docker_directory"/"$project_folder_name"/docker/src"
+
+# create the base directory
+mkdir -p $base_docker_directory
+
+#remove any files already in the $project_folder_name
+rm -rf $base_docker_directory/$project_folder_name
+
+# create the $project_folder_name
+mkdir $base_docker_directory/$project_folder_name
+
+echo "clone the project repository"
+
+#checkout the git projects into the same temporary docker directory
+git clone  $git_url $base_docker_directory/$project_folder_name
+
+echo "rename configuration files"
+
+#rename the query_metrics_calling_script.local.sql to query_metrics_calling_script.sql so it can be used as the active configuration file
+mv $full_project_path/SQL/query_metrics_calling_script.local.sql $full_project_path/SQL/query_metrics_calling_script.sql
+
+# remove the remote and hybrid scripts
+rm $full_project_path/SQL/query_metrics_calling_script.remote.sql
+
+rm $full_project_path/SQL/query_metrics_calling_script.hybrid.sql
+
+#rename the local oracle configuration file to be the active configuration file
+mv $full_project_path/oracle_configuration/tnsnames.local.ora $full_project_path/oracle_configuration/tnsnames.ora
+
+# remove the remote and hybrid oracle configuration files
+rm $full_project_path/oracle_configuration/tnsnames.remote.ora
+
+rm $full_project_path/oracle_configuration/tnsnames.hybrid.ora
+
+
+#rename the project_scenario_config.local.sh to project_scenario_config.sh so it can be used as the active configuration file
+mv $full_project_path/scripts/sh_script_config/project_scenario_config.local.sh $full_project_path/scripts/sh_script_config/project_scenario_config.sh
+
+# remove the remote and hybrid scripts
+rm $full_project_path/scripts/sh_script_config/project_scenario_config.remote.sh
+
+rm $full_project_path/scripts/sh_script_config/project_scenario_config.hybrid.sh
+
+# remove the preparation scripts:
+rm $base_docker_directory"/"$project_folder_name"/deployment_scripts/prepare_docker_project.local.sh"
+rm $base_docker_directory"/"$project_folder_name"/deployment_scripts/prepare_docker_project.hybrid.sh"
+rm $base_docker_directory"/"$project_folder_name"/deployment_scripts/prepare_docker_project.remote.sh"
+
+echo ""
+echo "the local docker project files are now ready for configuration and image building/deployment (press Enter key to continue)"
+
+read
