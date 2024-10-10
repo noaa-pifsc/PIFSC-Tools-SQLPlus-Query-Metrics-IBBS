ea19088801ac96d021ec39814fc148e69476ae4a
Jesse Abdul <jesse.abdul@noaa.gov>
2024-10-09 21:56:20 -1000

    bug fix

--- docker/src/scripts/traceroute.sh
+++ docker/src/scripts/traceroute.sh
@@ -11,7 +11,7 @@ echo "" >> ../logs/$2
 echo $(date "+%Y%m%d %I:%M:%S %p")" (UTC) - Start the traceroute">> ../logs/$2
 
 # log the command used for the traceroute
-echo "the command is traceroute -T -p 22 $1" >> ../logs/$2
+echo "the command is: traceroute -T -p 22 $1" >> ../logs/$2
 
 # send the traceroute command and save it to the log file
 traceroute -T -p 22 "$1" >> ../logs/$2
