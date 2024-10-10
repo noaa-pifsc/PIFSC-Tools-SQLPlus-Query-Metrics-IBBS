4d1d8a971ec8950fdad2e4153434607a441b00c6
Jesse Abdul <jesse.abdul@noaa.gov>
2024-10-10 11:51:05 -1000

    Removed on-prem east/west coast VPN scenarios

--- docker/src/SQL/sqlplus_config/scenario_config.west-vpn.local.sql
+++
@@ -1,5 +0,0 @@
---define the local variables to indicate the scenario the SQL*Plus script is being run
-DEFINE V_APP_LOCATION_NAME = 'Local';
-DEFINE V_DB_LOCATION_NAME = 'Local';
-DEFINE V_NETWORK_NAME = 'West Coast VPN';
-
