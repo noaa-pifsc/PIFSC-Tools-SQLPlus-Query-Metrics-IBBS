fd2d9540675a3f5c8d57681c06812076d3462f4d
Jesse Abdul <jesse.abdul@noaa.gov>
2024-10-11 08:59:46 -1000

    reverted development configuration

--- docker/src/scripts/sh_script_config/project_deploy_config.sh
+++ docker/src/scripts/sh_script_config/project_deploy_config.sh
@@ -4,5 +4,4 @@
 project_path="sqlplus-query-metrics-ibbs"
 
 # project git URL value
-git_url="--branch Branch_ibbs_sqlplus_query_metrics_v1.4 https://github.com/noaa-pifsc/PIFSC-Tools-SQLPlus-Query-Metrics-IBBS.git"
-
+git_url="https://github.com/noaa-pifsc/PIFSC-Tools-SQLPlus-Query-Metrics-IBBS.git"
