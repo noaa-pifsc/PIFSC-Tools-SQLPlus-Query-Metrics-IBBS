29d4f362f97db72e7854974eea72bd6b4ff90c3f
Jesse Abdul <jesse.abdul@noaa.gov>
2024-10-14 12:35:25 -1000

    added comments

--- performance reports/traceroutes/parse_traceroute.php
+++ performance reports/traceroutes/parse_traceroute.php
@@ -1,38 +1,22 @@
 <?php
 
-
-	//read all .log files in the directory and generate .csv files for each of them
-		//should we try to combine them all or set a column based on the name of the file prefix?
-		
-		//maybe parse each of the "." pieces of the string and add as columns?
-		//e.g. pacific-vpn (network), remote (scenario) then it can all be combined in a single file
-	
-	
-	
-
-//	$filename = 'pacific-vpn.remote_traceroute.20241011.log';
-
-
-	//construct the .csv file:
-	
+	//construct the .csv file header:
 	$csv_content="\"Scenario\",\"Network\",\"Destination\",\"Start Date/Time (UTC)\",\"End Date/Time (UTC)\",\"# of hops\",\"RTT Attempt 1 (ms)\",\"RTT Attempt 2 (ms)\",\"RTT Attempt 3 (ms)\"";
 
+	//open the current directory 
 	if ($handle = opendir('.')) 
 	{
 		
 //		echo "The current directory has been opened successfully\n";
-		
+		//read the directory and loop through the files 
 		while (false !== ($filename = readdir($handle))) {
-
-//			echo "The current filename is: $filename\n";
-
+			//check the current file
 
 			//check to see if the current file in the directory is a traceroute log file
 			if (preg_match('/([a-zA-Z\-]+)\.([a-zA-Z]+)\_traceroute\.[0-9]+\.log/', $filename, $matches))
 			{
 				//the current file is a traceroute log file
 				echo "the current file ($filename) in the directory is a traceroute log file\n";
-
 
 				//load the traceroute log file's content
 				$v_data = file_get_contents($filename);
@@ -53,7 +37,6 @@
 
 						//extract the current traceroute command 
 						$temp_match = substr($v_data, 0, ($total_offset = strlen($matches[0][0])+$matches[0][1]));
-						
 
 						//parse the current traceroute command to extract the statistics
 						preg_match('/([0-9]{8} [0-9]{2}\:[0-9]{2}\:[0-9]{2} (AM|PM)) \(UTC\) \- Start the traceroute.+traceroute to ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|[a-zA-Z0-9\.]+).+\ +([0-9]{1,})\ +[0-9a-zA-Z\(\. ]+\)\ +([0-9]+\.[0-9]+\ ms|\*)\ +([0-9]+\.[0-9]+\ ms|\*)\ +([0-9]+\.[0-9]+\ ms|\*).([0-9]{8} [0-9]{2}\:[0-9]{2}\:[0-9]{2} (AM|PM)) \(UTC\) \- Traceroute has ended/s', $temp_match, $matches);
@@ -63,23 +46,19 @@
 						
 						//remove the current traceroute command from the $v_data variable so the next command can be processed
 						$v_data = trim(substr($v_data, $total_offset));
-
-					}		
-
-
+					}
 				}
 				
 				echo "the current traceroute log file ($filename) has been processed\n";
-
 			}
-
-
 		}
 
 		echo "write the .csv file to the file system\n";
 
 		//write the csv file:
 		file_put_contents("parsed_traceroute_data.csv", $csv_content);
+
+		//close the directory handle
 		closedir($handle);
 	}
 
